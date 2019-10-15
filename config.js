2aa43bb56cafac116dcc35ba6d9878017b99d502
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-10-14 21:00:34 -0500

    Edited path prefix

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/portfolio`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/portfolio_githubpages`, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Xavier Puentes',
   heading: 'Full Stack Web Developer',
   // social
