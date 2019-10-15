e0dec0f3a7744bc9cc301431f0d4ef1547408d13
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-10-14 20:56:25 -0500

    Initial commit and push

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteTitle: 'Xavier Puentes', // <title>
+  manifestName: 'Prologue',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/portfolio`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Xavier Puentes',
+  heading: 'Full Stack Web Developer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/xpuentes',
+    },
+    {
+      icon: 'fa-linkedin-square',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/xavierpuentes/',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:xpuentes93@gmail.com',
+    },
+  ],
+};
